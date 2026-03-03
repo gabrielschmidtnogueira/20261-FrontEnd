82b494fe5cebb6ca73ebbdaa72904a4e8d3d081d
Rafael Moreno <rafaelmorenosax@gmail.com>
2026-03-02 21:20:51 -0300

    Aula 2

--- Aula 1/Aula1/src/app/app-module.ts
+++ Aula 1/Aula1/src/app/app-module.ts
@@ -7,13 +7,21 @@ import { MenuSuperior } from './menu-superior/menu-superior';
 import { Destaque } from './destaque/destaque';
 import { Cards } from './cards/cards';
 import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Home } from './home/home';
+import { Calculadora } from './calculadora/calculadora';
+import { Cliente } from './cliente/cliente';
+import { PaginaNaoEncontrada } from './pagina-nao-encontrada/pagina-nao-encontrada';
 
 @NgModule({
   declarations: [
     App,
     MenuSuperior,
     Destaque,
-    Cards
+    Cards,
+    Home,
+    Calculadora,
+    Cliente,
+    PaginaNaoEncontrada
   ],
   imports: [
     BrowserModule,
--- Aula 1/Aula1/src/app/app-routing-module.ts
+++ Aula 1/Aula1/src/app/app-routing-module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Calculadora } from './calculadora/calculadora';
+import { Cliente } from './cliente/cliente';
+import { PaginaNaoEncontrada } from './pagina-nao-encontrada/pagina-nao-encontrada';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'calculadora', component: Calculadora },
+  { path: 'cliente', component: Cliente },
+  { path: '**', component: PaginaNaoEncontrada }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
